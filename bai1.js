b72d635e64fe1fc11889529504c05d44f837963f
vantam22 <121233679+vantam06@users.noreply.github.com>
2023-10-10 18:25:27 +0700

    js

---
+++ bai1.js
@@ -0,0 +1,10 @@
+var fullName = 'Tran Van Tam'; //Khai báo biến họ tên
+var age = 21; //Khai báo biến tuổi
+var address = 'Lien Chieu, TP Da Nang'; //Khai báo biến địa chỉ
+
+setTimeout(function() {
+    alert(fullName);
+    alert(age);
+    alert(address);
+},2000)
+
---
+++ bai2.js
@@ -0,0 +1,5 @@
+
+var tuoi = prompt("Tuổi bạn là:");
+var hoten = prompt("Nhập họ tên:");
+console.log('Họ tên là: ' + hoten);
+console.log('Tuổi: ' + tuoi);
